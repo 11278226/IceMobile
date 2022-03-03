7e3deae468e1b43acc0b8966302776d31ecb3f7b
11278226 <iamdermo@gmail.com>
2022-03-03 15:01:08 +0100

    Basic app

---
+++ IceMobile/IceMobile/IceMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceMobileApp.swift
+//  IceMobile
+//
+//  Created by Diarmuid O'Keeffe on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct IceMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
